c503895a88abad474ae3dbd60dfb279c31608d50
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-02-17 15:11:54 +0530

    Updated the workdlow

---
+++ examples/transform_auto.rs
@@ -0,0 +1,48 @@
+use std::io::Cursor;
+use prk_async_dataflow::{AsyncJsonParser, DataConnector, FeatureTransformer, HttpConnector, ParserConfig};
+use serde::{Deserialize, Serialize};
+use simd_json::base::ValueAsScalar;
+use tokio_stream::StreamExt;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Post {
+    id: i64,
+    title: String,
+    body: String,
+}
+// transformer.rs
+#[tokio::main]
+async fn main() {
+    let connector = HttpConnector::new("https://jsonplaceholder.typicode.com/posts").unwrap();
+    let data = connector.fetch().await.unwrap();
+    let reader = Cursor::new(data);
+    
+
+    let mut transformer = FeatureTransformer::new();
+    transformer.add_mapping("title".to_string(), Box::new(|mut v| {
+        // Transform the title to uppercase
+        v = v.as_str().unwrap().to_uppercase().into();
+        v
+    }));
+
+    // let parser = AsyncJsonParser::new(reader, );
+    let parser = AsyncJsonParser::with_config(reader, ParserConfig {
+        batch_size: 5,
+        transformer: Some(transformer.into()),
+        ..Default::default()
+    });
+
+    let mut stream = parser.into_stream::<simd_json::OwnedValue>();
+    while let Some(result) = stream.next().await {
+        match result {
+            Ok(value) => {
+                // value = transformer.transform(value.into()).into();
+                match simd_json::serde::from_owned_value::<Post>(value) {
+                    Ok(post) => println!("Transformed Post: {:#?}", post),
+                    Err(e) => eprintln!("Deserialization error: {}", e),
+                }
+            }
+            Err(e) => eprintln!("Error parsing JSON: {}", e),
+        }
+    }
+}
